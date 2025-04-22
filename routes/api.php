16d56aeb78c2c48085dd210c1866505ea6e4fed4
Zakaria <zakaria.dev2022@gmail.com>
2025-04-22 16:16:55 +0100

    la partie metier pour la demande d'emploie & emploie

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\DemandeEmploiController;
-use App\Http\Controllers\EmploieController;
+use App\Http\Controllers\Api\EmploieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,12 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ************************************************************************************************************************
 Route::apiResource('demandes-emploi', DemandeEmploiController::class);
 Route::post('/demandes-emploi/{id}', [DemandeEmploiController::class, 'update']);//quand on a besoin de mettre à jour un objet ; la difference entre ajouter et modifier c'est l'id dans l'url
 Route::post('/demandes-emploi/{id}/accepte', [DemandeEmploiController::class, 'accepterDemande']);//acceptation de demande d'emploie
 // ************************************************************************************************************************
 Route::apiResource('emploies', EmploieController::class);
 Route::post('/emploies/{id}', [EmploieController::class, 'update']);//quand on a besoin de mettre à jour un objet ; la difference entre ajouter et modifier c'est l'id dans l'url
+// ************************************************************************************************************************
 
-
-        
